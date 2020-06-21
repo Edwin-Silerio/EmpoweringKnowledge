59355e1dd10baea0fd237023f53d253b97dbb27e
Edwin_Silerio <edwin_silerio@utexas.edu>
2020-06-21 15:49:17 -0500

    First commit

---
+++ frontend/empoweringknowledge/build/precache-manifest.2900651fa3be7200cb6c94ba610f542f.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1f9dae7e8f555c49d7445bba3d38439",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7af463bee171685fa0ae",
+    "url": "/static/css/2.484825d3.chunk.css"
+  },
+  {
+    "revision": "3070b3c179047e13fc7b",
+    "url": "/static/css/main.33cbb16d.chunk.css"
+  },
+  {
+    "revision": "7af463bee171685fa0ae",
+    "url": "/static/js/2.ba25364c.chunk.js"
+  },
+  {
+    "revision": "3070b3c179047e13fc7b",
+    "url": "/static/js/main.aef9536a.chunk.js"
+  },
+  {
+    "revision": "a3b121775eca90274d47",
+    "url": "/static/js/runtime-main.b58ff6fd.js"
+  },
+  {
+    "revision": "addd55df6ed8f65714b68c6194899b2f",
+    "url": "/static/media/CFM_BarChart.addd55df.jpg"
+  },
+  {
+    "revision": "074049d433f983631c668835c34b5738",
+    "url": "/static/media/Congress.074049d4.jpeg"
+  },
+  {
+    "revision": "beac9ac9b398dbe3c5c6018255f0a3ff",
+    "url": "/static/media/DonationsBarChart.beac9ac9.jpg"
+  },
+  {
+    "revision": "16f316f048c6202c2adb3c9cf66fbac1",
+    "url": "/static/media/Legislation.16f316f0.jpeg"
+  },
+  {
+    "revision": "4f7ef2ed477770b9e2ff04f660290dd4",
+    "url": "/static/media/ScatterPlot.4f7ef2ed.jpg"
+  },
+  {
+    "revision": "53abc765a38ffd4bea8103e1c74efd85",
+    "url": "/static/media/Windmills.53abc765.jpeg"
+  },
+  {
+    "revision": "1e60c651bb0cb13bb051e820aaca7fb9",
+    "url": "/static/media/bar_chart.1e60c651.jpg"
+  },
+  {
+    "revision": "8581a748b6fe31ad545481f297f6285d",
+    "url": "/static/media/briana.8581a748.jpg"
+  },
+  {
+    "revision": "484faf11030a9cf76e4079ef2270b5ee",
+    "url": "/static/media/donations_chart.484faf11.jpg"
+  },
+  {
+    "revision": "3bbb65984c4174bb1144068d9304608b",
+    "url": "/static/media/edwin.3bbb6598.jpeg"
+  },
+  {
+    "revision": "cadf2511370fd54be2b43f965f49a941",
+    "url": "/static/media/homepage.cadf2511.jpg"
+  },
+  {
+    "revision": "3d569f6aa2ed7e86d24b2091830d9fe7",
+    "url": "/static/media/ojas.3d569f6a.jpg"
+  },
+  {
+    "revision": "a3f0d0f3c5256a18448b531d05aea2e9",
+    "url": "/static/media/ryan.a3f0d0f3.jpg"
+  },
+  {
+    "revision": "62af612609aecccae4d8805c820cc726",
+    "url": "/static/media/travis.62af6126.jpeg"
+  }
+]);
